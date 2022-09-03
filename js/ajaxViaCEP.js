69f7e9626852a020680f52b40d054f79a4a2b62f
Guest 01 <guest01@alunos.sc.senac.br>
2022-09-02 21:48:16 -0300

    02-09-22

--- js/ajaxViaCEP.js
+++ js/ajaxViaCEP.js
@@ -1,5 +1,26 @@
 
 function submitCep(){
-    alert('olá vc esa submetendo o cep');
+    //alert('olá vc esa submetendo o cep');
     //console.log('olá vc esa submetendo o cep');
+    const cep = $("input#cep").val();
+    $.ajax({
+        type:"GET",
+        url:"https://viacep.com.br/ws/"+cep+"/json/",
+        //url:"https://localhost:8080/api/transid/vacina"
+        dataType:"json",
+        success:function(data){
+            //console.log(data);
+            const ele = '<div>' +
+                        '<span>logradouro:' + data.logradouro + '</span>' +
+                        '</div>'
+            $(ele).appendTo("#janela-conteudo");
+        },
+        erro:function(){
+            alert("Deu algo de errado");
+        },
+        beforeSend:function(){
+            
+        }
+        
+    })
 }
