12246291f751730c3b4006b1b0c67c1f5cdd9cd1
Guest 01 <guest01@alunos.sc.senac.br>
2022-09-09 22:01:39 -0300

    09-09-22

--- js/ajaxIBGE.js
+++ js/ajaxIBGE.js
@@ -7,7 +7,7 @@ function getEstado(){
         success:function(data){            
             let option = '<option value="">Selecione um estado</option>'
             $.each(data, function(i,e){
-                option += `<option value="${e.sigla}" id="${e.id}>${e.nome}</option>`
+                option += `<option value="${e.sigla}" id="${e.id}">${e.nome}</option>`
             });
             $(option).appendTo("#estado");
         },
@@ -19,4 +19,38 @@ function getEstado(){
         }
         
     })
+}
+function getCidade(uf){    
+    $.ajax({
+        type:"GET",
+        url:"https://servicodados.ibge.gov.br/api/v1/localidades/estados/"+uf+"/municipios",        
+        dataType:"json",
+        success:function(data){            
+            getNewElementCidade(data);
+        },
+        erro:function(){
+            
+        },
+        beforeSend:function(){
+            
+        }
+        
+    })
+}
+function getActionEstado(){
+    const uf = $("#estado").find(":selected").attr("value");
+    getCidade(uf);
+}
+function getNewElementCidade(data){
+    $("#label-Cidade").remove();
+    $("#cidade").remove();
+
+    let select = '<label id="label-Cidade">Selecione a Cidade: </label>'+
+                 '<select id="cidade"></select>';
+    let option = "";    
+    $.each(data, function(i, e){
+        option += `<option value="${e.nome}">${e.nome}</option>`
+    })
+    $(select).appendTo("#janela-conteudo");
+    $(option).appendTo("#cidade");
 }
--- js/sistemaJanela.js
+++ js/sistemaJanela.js
@@ -1,5 +1,6 @@
+const menu = "#menu-ativo";
 function start(){
-    const menu = "#menu-ativo";
+    
     if($(menu).is(":visible")){
         esconderElemento($(menu));
     }else{
@@ -13,29 +14,43 @@ function mostrarElemento(ele){
     $(ele).show();
 }
 function abrirJanela(ele){
-    console.log($(ele));
+    //console.log($(ele));
+
+    esconderElemento($(menu));
+
+    $("#janela-conteudo").find(".dados:not(.template)").remove();
+    let dados = $("#janela-conteudo").find(".dados.template").clone().removeClass("template");
+
+    $("#janela").find("#icone span").remove();
+
     if($(ele).hasClass("tabuada")){
-       $("#janela").find("#janela-conteudo").text("tabuada");
+        $("<span class='material-symbols-outlined'>calculate</span>");
+        $("#janela").find("#title").text("Tabuada");
+        $(dados).text("tabuada");
     }
     if($(ele).hasClass("imc")){
-        $("#janela").find("#janela-conteudo").text("imc");
+        $("#janela").find("#title").text("IMC");
+        $(dados).text("IMC");
     }
     if($(ele).hasClass("calculadora")){
-        $("#janela").find("#janela-conteudo").text("calculadora");
+        $("#janela").find("#title").text("Calculadora");
+        $(dados).text("Calculadora");
     }
     if($(ele).hasClass("ajax")){
         $("#janela").find("#title").text("Exemplo AJAX");
         const form ='<label>CEP:</label>' +
                     '<input type="text" id="cep">' +
                     '<button id="enviar_cep">Enviar</button>';
-        $(form).appendTo("#janela-conteudo");
+        $(form).appendTo(dados);
     }
     if($(ele).hasClass("ibge")){
+        $("<span class='material-symbols-outlined'>location_on</span>").appendTo($("#janela").find("#icone span"));
         $("#janela").find("#title").text("Exemplo IBGE");
         const select = '<label>Selecione um estado:</label>' +
-                       '<select id="estado"></select>';
-        $(select).appendTo("#janela-conteudo");
+                       '<select id="estado" onchange="getActionEstado()"></select>';
+        $(select).appendTo(dados);
         getEstado();
     }
+    $(dados).appendTo("#janela-conteudo");
     $('#janela').show();
 }
